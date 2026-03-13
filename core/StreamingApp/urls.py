f418d7fdb58a4c8e69332b2c1a209fbfd9b3efda
Qiuraa <gtmagic20@gmail.com>
2026-03-13 13:50:05 +0700

    change password done

--- core/StreamingApp/urls.py
+++ core/StreamingApp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
@@ -48,5 +49,35 @@ urlpatterns = [
     path('watchlist/', views.view_watchlist, name='watchlist'),
     path('watchlist/remove/<uuid:series_id>/', views.remove_from_watchlist, name='remove_from_watchlist'),
     path('comment/<uuid:series_id>/<uuid:episode_id>/', views.viewer_comment, name='viewer_comment'),
-    # legacy/unused patterns removed; use the above simple endpoints
+    path(
+        'password-reset/',
+        auth_views.PasswordResetView.as_view(
+            template_name='auth/password_reset.html'
+        ),
+        name='password_reset'
+    ),
+
+    path(
+        'password-reset/done/',
+        auth_views.PasswordResetDoneView.as_view(
+            template_name='auth/password_reset_done.html'
+        ),
+        name='password_reset_done'
+    ),
+
+    path(
+        'reset/<uidb64>/<token>/',
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name='auth/password_reset_confirm.html'
+        ),
+        name='password_reset_confirm'
+    ),
+
+    path(
+        'reset/done/',
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name='auth/password_reset_complete.html'
+        ),
+        name='password_reset_complete'
+    ),
 ]
--- core/settings.py
+++ core/settings.py
@@ -127,6 +127,16 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 AUTH_USER_MODEL = 'StreamingApp.User'
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'Kingpakan302@gmail.com'
+EMAIL_HOST_PASSWORD = 'whjrredycwobuehj'
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+# settings.py
+
+PASSWORD_RESET_TIMEOUT = 3600
 # LOGIN_URL = 'admin_login'
 # # After login, redirect admin users to the admin homepage by default
 # LOGIN_REDIRECT_URL = 'admin_homepage'
